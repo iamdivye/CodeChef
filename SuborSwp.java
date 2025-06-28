a7913a80e2520826dc401397bc2b8bf16949d56b
iamdivye <divyejaiswal5@gmail.com>
2025-06-29 05:26:14 +0530

    Sub or Swp

---
+++ SuborSwp.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SuborSwp {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(gcd(a, b));
+        }
+        in.close();
+    }
+    
+    private static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
