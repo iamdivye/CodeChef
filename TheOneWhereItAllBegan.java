370246a7069d2c2959b3988676a6eef661e04b20
iamdivye <divyejaiswal5@gmail.com>
2025-07-30 20:47:25 +0530

    https://www.codechef.com/problems/S01E01

---
+++ TheOneWhereItAllBegan.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in=new Scanner(System.in);
+		int t=in.nextInt();
+		while(t-->0){
+		    int n=in.nextInt();
+		    if(n>=21)
+		    System.out.println("YES");
+		    else
+		    System.out.println("NO");
+		}
+
+	}
+}
