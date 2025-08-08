cc483c24a5b566e2d1f6321d0386cd6c3b987551
iamdivye <divyejaiswal5@gmail.com>
2025-08-08 20:16:55 +0530

    https://www.codechef.com/problems/HJJ

---
+++ CanYouBench.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt();
+        System.out.println(X + 20);
+    }
+}
