083596d7cf2607dda97c1d2616616f6f54d7e4a6
iamdivye <divyejaiswal5@gmail.com>
2025-08-28 23:14:45 +0530

    https://www.codechef.com/problems/FARAWAY?tab=statement

---
+++ FarAway.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            long ans = 0;
+
+            for (int i = 0; i < N; i++) {
+                int Ai = sc.nextInt();
+                ans += Math.max(Ai - 1, M - Ai);
+            }
+
+            System.out.println(ans);
+        }
+
+    }
+}
