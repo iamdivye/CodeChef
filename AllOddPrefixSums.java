cf43d9e66c1d9b2663bcff18fcec26a38c91b17c
iamdivye <divyejaiswal5@gmail.com>
2025-09-22 22:01:55 +0530

    https://www.codechef.com/problems/ODDPREFSUM

---
+++ AllOddPrefixSums.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            int oddCount = 0;
+
+            for (int i = 0; i < N; i++) {
+                int val = sc.nextInt();
+                if (val % 2 != 0) oddCount++;
+            }
+
+            if (oddCount == 1) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+
+        sc.close();
+    }
+}
