3def54bd6952b67f39c50f7dad71dc82a7b18608
iamdivye <divyejaiswal5@gmail.com>
2025-10-07 23:03:44 +0530

    https://www.codechef.com/problems/EQCARDGAME

---
+++ EqualCardGame.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int K = sc.nextInt();
+            System.out.println(52 % K);
+        }
+        sc.close();
+    }
+}
